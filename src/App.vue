679c45d8e7a4039fda761a45c582cf7b5cf956c4
Moh Zaman <moh.zaman@corestream.co.uk>
2024-09-20 11:30:16 +0100

    Dark Background Sorted with Image

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
-
+<div class="background-header-div">
+  <img 
+  :src="bgImageDark"
+  class="background-header-image"/>
+</div>
+<div class="task-list-wrapper">
+  <TaskList/>
+</div>
 </template>
 
 <script>
+import bgImageDark from "./assets/images/bg-desktop-dark.jpg"
+import TaskList from "./components/TaskList.vue";
+
   export default {
     name: "App",
+    data() {
+      return {
+        bgImageDark: bgImageDark
+      }
+    },
+    components: {
+      TaskList,
+    },
   }
 </script>
 
 <style>
 body {
-  background-color: black;
+  background-color: hsl(235, 21%, 11%)
 }
+
+.background-header-div {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+.background-header-image {
+  width: 100%;
+}
+
+.task-list-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="header-title-text">T O D O</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TaskList" 
+    }
+</script>
+
+<style >
+.header-title-text {
+    color: white;
+    font-weight: 700;
+}
+</style>
