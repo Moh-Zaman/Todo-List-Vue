709c4361647f525e82ec5f9fa313e30f6c7b5068
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-12-11 21:13:23 +0000

    Cleaned up code a little

--- src/classes/TodoListClass.js
+++ src/classes/TodoListClass.js
@@ -1,39 +1,36 @@
 export class TodoListClass {
-    constructor(tasks = []) {
-        this.tasks = tasks
+  constructor(tasks = []) {
+    this.tasks = tasks;
+  }
+
+  addTaskHandler(newTask) {
+    this.tasks.push({
+      id: this.randomNumberGen(),
+      name: newTask,
+      complete: false,
+    });
+  }
+
+  parentDeleteTasks() {
+    this.tasks = this.tasks.filter((task) => !task.complete);
+  }
+
+  parentToggleCompleted(id) {
+    const i = this.tasks.findIndex((task) => task.id === id);
+    if (i !== -1) {
+      this.tasks[i].complete = !this.tasks[i].complete;
     }
+  }
 
-    addTaskHandler(newTask) {
-        const trimmedName = newTask.trim();
-        if (trimmedName) {
-          this.tasks.push({
-            id: this.randomNumberGen(),
-            name: trimmedName,
-            complete: false,
-          });
-        }
-      }
+  parentDeleteOneTask(id) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 
-      parentDeleteTasks() {
-        this.tasks = this.tasks.filter((task) => !task.complete);
-      }
+  toggleAllTasks(checked) {
+    this.tasks.forEach((task) => (task.complete = checked));
+  }
 
-      parentToggleCompleted(id) {
-        const i = this.tasks.findIndex((task) => task.id === id);
-        if (i !== -1) {
-          this.tasks[i].complete = !this.tasks[i].complete;
-        }
-      }
-
-      parentDeleteOneTask(id) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
-      }
-
-      toggleAllTasks(checked) {
-        this.tasks.forEach((task) => (task.complete = checked));
-      }
-
-      randomNumberGen() {
-        return Date.now() + Math.floor(Math.random());
-      }
+  randomNumberGen() {
+    return Date.now() + Math.floor(Math.random());
+  }
 }
